4a3a12f2342a9f063b552760b5ce46ec15c82580
Soniya Dasadia <110859121+soniya-d@users.noreply.github.com>
2025-01-10 10:30:23 -0500

    Update person.java

--- person.java
+++ person.java
@@ -1,21 +1,17 @@
 
-public class person {
-    // protected fields to allow subclasses to directly access these values
+public class Person {
     protected String id;  
     protected String name; 
 
-    // initialize person
-    public person(String id, String name) {
-        this.id = id;   // Assign the ID
-        this.name = name; // Assign the name
+    public Person(String id, String name) {
+        this.id = id;   
+        this.name = name; 
     }
 
-    // getter method to get person's ID
     public String getId() { 
         return id; 
     }
 
-    // getter method to get person's name
     public String getName() { 
         return name; 
     }
